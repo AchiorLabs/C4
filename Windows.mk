4f75daca190cdbfc1b8f28870777e02862261c1a
AllenK-1 <alienware105@proton.me>
2026-04-27 18:14:04 +0000

    Complete Windows.mk file as it was missing several objects

--- Windows.mk
+++ Windows.mk
@@ -8,6 +8,55 @@ OUTOBJ=/Fo:
 OUTEXE=/Fe:
 
 
+OBJECTS= \
+$(OBJ)/Support/BumpAllocator.obj \
+$(OBJ)/Support/String.obj \
+$(OBJ)/Support/Log.obj \
+$(OBJ)/Support/List.obj \
+$(OBJ)/CmdLine/CmdLineArgumentList.obj \
+$(OBJ)/CmdLine/CmdLine.obj \
+$(OBJ)/C4COptions/C4COptions.obj \
+$(OBJ)/FrontEnd/Lexer/Token.obj \
+$(OBJ)/FrontEnd/Lexer/Lexer.obj \
+$(OBJ)/FrontEnd/AST/AST.obj \
+$(OBJ)/FrontEnd/AST/ASTPrint.obj \
+$(OBJ)/FrontEnd/Parser/Parser.obj \
+$(OBJ)/FrontEnd/TreeWalker/TreeWalker.obj \
+$(OBJ)/FrontEnd/Driver/Driver.obj \
+$(OBJ)/MiddleEnd/C/ASTToC.obj \
+$(OBJ)/MiddleEnd/Driver/Driver.obj \
+$(OBJ)/Driver/Driver.obj
+
+
+$(TARGET)/Main.exe: $(OBJECTS)
+	$(CC) $^ /Fe:$(TARGET)/Main.exe
+
+
+$(OBJ)/Support/BumpAllocator.obj: $(SRC)/Support/BumpAllocator.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/Support/String.obj: $(SRC)/Support/String.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/Support/Log.obj: $(SRC)/Support/Log.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/Support/List.obj: $(SRC)/Support/List.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+
+$(OBJ)/CmdLine/CmdLineArgumentList.obj: $(SRC)/CmdLine/CmdLineArgumentList.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/CmdLine/CmdLine.obj: $(SRC)/CmdLine/CmdLine.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/C4COptions/C4COptions.obj: $(SRC)/C4COptions/C4COptions.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
+$(OBJ)/FrontEnd/Lexer/Token.obj: $(SRC)/FrontEnd/Lexer/Token.c
+	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
+
 $(OBJ)/FrontEnd/Lexer/Lexer.obj: $(SRC)/FrontEnd/Lexer/Lexer.c
 	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
 
@@ -37,28 +86,6 @@ $(OBJ)/MiddleEnd/Driver/Driver.obj: $(SRC)/MiddleEnd/Driver/Driver.c
 $(OBJ)/Driver/Driver.obj: $(SRC)/Driver/Driver.c
 	$(CC) $(CFLAGS) $< $(OUTOBJ)$@
 
-# Linking the final executable (Main.exe)
-
-$(TARGET)/Main.exe: \
-$(OBJ)/FrontEnd/Lexer/Lexer.obj \
-$(OBJ)/FrontEnd/AST/AST.obj \
-$(OBJ)/FrontEnd/AST/ASTPrint.obj \
-$(OBJ)/FrontEnd/Parser/Parser.obj \
-$(OBJ)/FrontEnd/TreeWalker/TreeWalker.obj \
-$(OBJ)/FrontEnd/Driver/Driver.obj \
-$(OBJ)/MiddleEnd/C/ASTToC.obj \
-$(OBJ)/MiddleEnd/Driver/Driver.obj \
-$(OBJ)/Driver/Driver.obj
-	$(CC) $^ /Fe:$(TARGET)/Main.exe
-
-# Ensure Target directory exists
-$(TARGET):
-	if not exist $(TARGET) mkdir $(TARGET)
-
-# Default build target
-all: $(TARGET) $(TARGET)/Main.exe
-
-# --- Clean ---
 
 clean:
 	if exist $(OBJ) rmdir /s /q $(OBJ)
