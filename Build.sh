ca1e3c35bc52f1f8f1a1a172c2b30d41d96ab8e4
AllenK-1 <alienware105@proton.me>
2026-04-27 17:52:03 +0000

    Resolved Build.sh

--- Build.sh
+++ Build.sh
@@ -1,7 +1,4 @@
 make clean
-mke
-$src = "Target/Main.exe"
-$dest = "C:\AchiorLabs\bin\C4C.exe"
-New-Item -ItemType Directory -Path "C:\AchiorLabs\bin" -Force | Out-Null
-Copy-Item $src $dest -Force
-Write-Host "Installed C4C to $dest"
+make
+
+sudo cp Target/Main /usr/bin/C4C
